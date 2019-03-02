d81f6d35c80fed2e3a9573324f7c1950fad0ca2a
YVEN SHANE <yvenshane@aol.com>
2019-03-02 16:58:09 +0800

    Create project

---
+++ CiLuNetwork2/CiLuNetwork2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CiLuNetwork2
+//
+//  Created by YVEN on 2019/3/2.
+//  Copyright © 2019 Hefei Haiba Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
